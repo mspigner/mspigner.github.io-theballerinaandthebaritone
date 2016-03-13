d62a35b883d4980a833108776b159cb6c88dbf4c
mspigner <mspigner@Michelles-MacBook-Pro.local>
2016-03-12 22:02:39 -0500

    Bio Video and Images
    Includes Mobile Optimization

--- js/main.js
+++ js/main.js
@@ -30,3 +30,86 @@ $('.nav_button').on('click', function(){
  //        // window.scrollTo(0,615)});
 	// }
 });
+
+// GLOBAL VARIABLES	
+	// image urls - set up an array
+	var images = ["images/Engagement/engagement_1.jpg", "images/Engagement/engagement_2.jpg", "images/Engagement/engagment_3.jpg", "images/Engagement/engagement_4.jpg", "images/Engagement/engagement_5.jpg"];
+
+	// set all image likes at 0
+	// var likes = [0,0,0,0,0,0];
+	
+	// keep track of current position
+	var currentImage = 0;
+
+
+// when the user clicks on "next" button
+$('#next').on('click', function () {
+	// Add 1 to the current image position
+	currentImage += 1;
+	console.log(currentImage);
+	// Enable previous button
+	$('#prev').prop('disabled',false);
+	// Display the next image
+	$('#image_show').attr('src', images[currentImage]);
+	// // Show likes that correspond to image
+	// $('#votes').html('Likes: ' + likes[currentImage]);
+	// // If the user is on the last image (image 6, position 5)
+	if (currentImage === 5) {
+		//Disable next button
+		$('#next').prop('disabled','true')
+	}
+});
+	
+
+
+// when the user clicks the "previous" button
+$('#prev').on('click', function () {
+	// Enable next button
+	$('#next').prop('disabled',false);
+	// Subtract 1 from current image position
+	currentImage -= 1;
+	// Display the previous image
+	$('#image_show').attr('src', images[currentImage]);
+	// Display the votes that correspond with the image
+	// $('#votes').html('Likes: ' + likes[currentImage]);
+
+	// If the user is on the first image (image 1, position 0)
+	if (currentImage === 0){
+		// Disable previous image
+		$('#prev').prop('disabled',true);
+	}
+});
+
+
+// when user clicks on engagement image
+$('#play_button').on('click',function () {
+	$('#engagement_video_img').addClass('hide');
+	$('iframe').addClass('show');
+});
+
+$('.sidebar .li_data').on('click', function(){
+	// event.preventDefault();
+	var matchingDiv = $(this).data('section');
+	console.log(matchingDiv);
+	 $('html, body').animate({scrollTop: $('.' + matchingDiv).offset().top +300}, 1200);
+	// if (matchingDiv === "resume_section") {
+	// 	 $('html, body').animate({scrollTop: $('.' + matchingDiv).offset().top}, 1200);
+ //        // window.scrollTo(0,615)});
+	// }
+});
+
+$('.hamburger').on('click',function(){
+	$('body').addClass('active');
+	$('.nav').addClass('hide');
+});
+
+$('.sidebar p').on('click', function (e){
+	e.preventDefault();
+	$('body').removeClass('active');
+	$('.nav').removeClass('hide');
+});
+
+
+
+
+
